3fc98e6eec562fde9eb2e9c7d9ded9549c1fc52e
nihility1214 <nihility1214@163.com>
2022-10-31 20:01:53 +0800

    1031mybatis

---
+++ src/CSUSoftWare21/web/projectJPetStore/persistence/MybatisDemo.java
@@ -0,0 +1,34 @@
+package CSUSoftWare21.web.projectJPetStore.persistence;
+
+import CSUSoftWare21.web.projectJPetStore.domain.Account;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class MybatisDemo {
+    public static void main(String[] args) throws IOException {
+        //1.加载核心配置文件
+        //定义配置文件路径
+        String resource = "mybatis-config.xml";
+        //获取InputStream
+        //Resources是mybatis提供的资源加载类
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        //通过SqlSessionFactoryBuilder对象的build方法把流传进来
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        //2.获取SqlSession对象,用它执行sql语句
+        SqlSession sqlSession=sqlSessionFactory.openSession();
+
+        //3.执行sql
+        List<Account> accounts=sqlSession.selectList("test.selectAll");
+
+        System.out.println(accounts);
+
+        sqlSession.close();
+    }
+}
--- src/CSUSoftWare21/web/projectJPetStore/web/servlet/MainFormServlet.java
+++ src/CSUSoftWare21/web/projectJPetStore/web/servlet/MainFormServlet.java
@@ -1,10 +1,14 @@
 package CSUSoftWare21.web.projectJPetStore.web.servlet;
-
-import jakarta.servlet.ServletException;
+/*1031 这里一直报错，按提示导入了javax里的*/
+/*import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+*/
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class MainFormServlet extends HttpServlet {
