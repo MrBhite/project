00b74cc252314ff918c59d024014c972b802cb0c
nihility1214 <nihility1214@163.com>
2022-11-04 11:00:19 +0800

    1104

--- src/CSUSoftWare21/web/projectJPetStore/Log4JInit.java
+++ src/CSUSoftWare21/web/projectJPetStore/Log4JInit.java
@@ -43,18 +43,13 @@ public class Log4JInit extends HttpServlet {
         super.init(config);
     }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
+
     }
 
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
+
     }
 
 }
