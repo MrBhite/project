5d9311db76d41e532f2fee4ac83c4a689a8c2f20
nihility1214 <nihility1214@163.com>
2022-11-04 10:56:51 +0800

    1104

--- src/CSUSoftWare21/web/projectJPetStore/Log4JInit.java
+++ src/CSUSoftWare21/web/projectJPetStore/Log4JInit.java
@@ -17,23 +17,26 @@ public class Log4JInit extends HttpServlet {
         super();
     }
     public void init(ServletConfig config) throws ServletException {
-        System.out.println("Log4JInitServlet 正在初始化 log4j日志设置信息");
-        String log4jLocation = config.getInitParameter("log4j-properties-location");
+        System.out.println("初始化 log4j日志设置信息");
+        /*读出配置文件，然后调用configure函数。
+        一、文件在哪
+        二、正确的文件类型*/
+        String log4jLocation = config.getInitParameter("log4j-properties");
 
         ServletContext sc = config.getServletContext();
 
         if (log4jLocation == null) {
-            System.err.println("*** 没有 log4j-properties-location 初始化的文件, 所以使用 BasicConfigurator初始化");
+            System.err.println("没有 log4j-properties 初始化的文件, 使用 BasicConfigurator初始化");
             BasicConfigurator.configure();
         } else {
             String webAppPath = sc.getRealPath("/");
             String log4jProp = webAppPath + log4jLocation;
-            File yoMamaYesThisSaysYoMama = new File(log4jProp);
-            if (yoMamaYesThisSaysYoMama.exists()) {
+            File ori = new File(log4jProp);
+            if (ori.exists()) {
                 System.out.println("使用: " + log4jProp+"初始化日志设置信息");
                 PropertyConfigurator.configure(log4jProp);
             } else {
-                System.err.println("*** " + log4jProp + " 文件没有找到， 所以使用 BasicConfigurator初始化");
+                System.err.println(log4jProp + " 文件未找到， 使用 BasicConfigurator初始化");
                 BasicConfigurator.configure();
             }
         }
--- src/CSUSoftWare21/web/projectJPetStore/Log4JTest.java
+++ src/CSUSoftWare21/web/projectJPetStore/Log4JTest.java
@@ -14,26 +14,25 @@ import java.io.IOException;
 
 public class Log4JTest extends HttpServlet {
     /*1.得到记录器*/
-    private static final long serialVersionUID = 1L;
     private static Logger logger = Logger.getLogger(Log4JTest.class);
 
 
     public Log4JTest() {
+        super();
     }
 
     public void init(ServletConfig config) throws ServletException {
-        BasicConfigurator.configure ();
+
 
     }
 
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // 记录debug级别的信息
-        logger.debug("This is debug message.");
+
         // 记录info级别的信息
-        logger.info("This is info message.");
+        logger.info("info message.");
         // 记录error级别的信息
-        logger.error("This is error message.");
+        logger.error("error message.");
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
